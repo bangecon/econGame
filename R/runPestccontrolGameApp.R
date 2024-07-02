2e0170b740fd1760357080bab88ab65c4e23029c
bangecon <bangjamest@sau.edu>
2024-07-02 15:44:26 -0500

    pestcontrolGame

--- R/runPestccontrolGameApp.R
+++ R/runPestccontrolGameApp.R
@@ -1,5 +1,5 @@
 #' @export
-runAnchoringGameApp <- function() {
+runPestcontrolGameApp <- function() {
   appDir <- system.file("shiny-examples", "pestcontrolGameApp", package = "econGame")
   if (appDir == "") {
     stop("Could not find example directory. Try re-installing `econGame`.", call. = FALSE)
